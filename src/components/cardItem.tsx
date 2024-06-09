2a4c4bc450c082522c31205b9ec424c2ae395544
Langgeng Kanugrahan <lkanugrahan@gmail.com>
2024-06-09 10:00:23 +0700

    feat: resize

--- src/components/cardItem.tsx
+++ src/components/cardItem.tsx
@@ -10,15 +10,15 @@ handleName:(text:string)=>void
 const CardItem = ({data,handleName}:Props) => {
   return (
     <Card
-    className="flex flex-col sm:flex-col gap-2 sm:justify-center items-center w-full sm:w-64 sm:h-64 p-2"
+    className="flex flex-col gap-2 sm:justify-center items-center w-fit md:w-64 md:h-64 p-2"
     onClick={() => handleName(data.name)}
   >
     <Image
       src={data.image}
       alt={data.name}
-      className="object-contain w-40 sm:w-64 h-40 sm:h-64"
+      className="object-contain w-40 md:w-64 h-40 md:h-64"
     />
-    <Typography className="font-poppins font-semibold text-lg text-black">
+    <Typography className="font-poppins  font-semibold text-lg text-black text-center">
       {data.name}
     </Typography>
   </Card>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,14 +4,16 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <link
+        <title>Pundi Craft</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Secular+One&display=swap"
           rel="stylesheet"
         ></link>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
       </Head>
     </Html>
   );
